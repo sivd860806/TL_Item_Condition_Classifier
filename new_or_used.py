3742e9be5419c97f081db3fe7e810afbf1571da7
Sergio V <sivd06@gmail.com>
2026-05-03 16:08:13 -0500

    chore: versionar new_or_used.py (provisto, NO modificar) + PDF del assessment

---
+++ new_or_used.py
@@ -0,0 +1,63 @@
+"""
+Exercise description
+--------------------
+
+Description:
+In the context of Mercadolibre's Marketplace an algorithm is needed to predict if an item listed in the markeplace is new or used.
+
+Your tasks involve the data analysis, designing, processing and modeling of a machine learning solution 
+to predict if an item is new or used and then evaluate the model over held-out test data.
+
+To assist in that task a dataset is provided in `MLA_100k.jsonlines` and a function to read that dataset in `build_dataset`.
+
+For the evaluation, you will use the accuracy metric in order to get a result of 0.86 as minimum. 
+Additionally, you will have to choose an appropiate secondary metric and also elaborate an argument on why that metric was chosen.
+
+The deliverables are:
+--The file, including all the code needed to define and evaluate a model.
+--A document with an explanation on the criteria applied to choose the features, 
+  the proposed secondary metric and the performance achieved on that metrics. 
+  Optionally, you can deliver an EDA analysis with other formart like .ipynb
+
+
+
+"""
+
+import json
+
+
+# You can safely assume that `build_dataset` is correctly implemented
+def build_dataset():
+    with open("MLA_100k.jsonlines") as f:
+        data = [json.loads(x) for x in f]
+    target = lambda x: x.get("condition")
+    N = -10000
+    X_train = data[:N]
+    X_test = data[N:]
+    y_train = [target(x) for x in X_train]
+    y_test = [target(x) for x in X_test]
+    for x in X_test:
+        del x["condition"]
+    return X_train, y_train, X_test, y_test
+
+
+if __name__ == "__main__":
+    print("Loading dataset...")
+    # Train and test data following sklearn naming conventions
+    # X_train (X_test too) is a list of dicts with information about each item.
+    # y_train (y_test too) contains the labels to be predicted (new or used).
+    # The label of X_train[i] is y_train[i].
+    # The label of X_test[i] is y_test[i].
+    X_train, y_train, X_test, y_test = build_dataset()
+
+    assert len(X_train) == len(y_train) == 90000
+    assert len(X_test) == len(y_test) == 10000
+    assert all("condition" not in x for x in X_test)
+    assert set(y_train) <= {"new", "used", None}
+    print(f"X_train: {len(X_train)} | X_test: {len(X_test)}")
+    print(f"y_train class balance: new={y_train.count('new')}, used={y_train.count('used')}")
+
+    # Insert your code below this line:
+    # ...
+
+
